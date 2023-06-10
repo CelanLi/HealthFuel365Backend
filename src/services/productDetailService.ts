8badf3f3f652f7056f9300466eb4d77a0dfd9858
Yiheng Zhu <ge38rob@mytum.de>
2023-06-10 12:32:29 +0200

    detail page refinement

--- src/services/productDetailService.ts
+++ src/services/productDetailService.ts
@@ -5,16 +5,18 @@ import { ProductInterface } from "../models/product";
 import { findProductByID } from "./productService";
 export const findDetailByID = async (
   id: string
-): Promise<[ProductInterface, ProductDetailInterface] | null> => {
+): Promise<[ProductInterface | null, ProductDetailInterface | null] | null> => {
   console.log("in detail interface");
   const productDetail = await ProductDetailSchema.findOne({
     productID: id,
   });
   const product = await findProductByID(id);
   console.log(JSON.stringify(productDetail));
-  if (product == null || productDetail == null) {
-    return null;
-  } else {
+  if (product != null && productDetail == null) {
+    return [product, null];
+  } else if (product != null && productDetail != null) {
     return [product, productDetail];
+  } else {
+    return null;
   }
 };
