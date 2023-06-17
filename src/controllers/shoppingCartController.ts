0d4d1c78d2ab470500a1d694783d228ec34d8f3d
Liping Yang <ge38hon@mytum.de>
2023-06-17 23:59:58 +0800

    add promo code validation and remove function

--- src/controllers/shoppingCartController.ts
+++ src/controllers/shoppingCartController.ts
@@ -4,6 +4,8 @@ import {
   getShoppingCart,
   deleteProduct,
   changeProductQuantity,
+  validatePromoCode,
+  removePromoCode,
 } from "../services/shoppingCartService";
 
 export async function getShoppingCartList(req: Request, res: Response) {
@@ -44,7 +46,7 @@ export async function deleteProductItem(req: Request, res: Response) {
 
 export async function changeProductCount(req: Request, res: Response) {
   let { shoppingCartID, productID, quantity } = req.body;
-  try { 
+  try {
     //error
     if (!shoppingCartID || typeof shoppingCartID != "string") {
       return res
@@ -54,7 +56,11 @@ export async function changeProductCount(req: Request, res: Response) {
       return res
         .status(400)
         .json(badRequestErrorMessage("Missing body parameter productID"));
-    } else if (typeof quantity != "number" || `${quantity}`.includes('.') ||quantity < 1) {
+    } else if (
+      typeof quantity != "number" ||
+      `${quantity}`.includes(".") ||
+      quantity < 1
+    ) {
       return res
         .status(400)
         .json(
@@ -68,3 +74,45 @@ export async function changeProductCount(req: Request, res: Response) {
     return res.status(500).json(internalServerErrorMessage);
   }
 }
+
+export async function validateCode(req: Request, res: Response) {
+  let { shoppingCartID, code } = req.body;
+  try {
+    //error
+    if (!code || typeof code != "string") {
+      return res
+        .status(400)
+        .json(badRequestErrorMessage("Missing body parameter Promo Code"));
+    }
+    console.log(shoppingCartID, code);
+
+    try {
+      await validatePromoCode(shoppingCartID, code)
+      return res.status(200).send({ code: 0, message: "sucess" });
+    } catch(err) {
+      return res.status(200).send({ code: -1, message: err });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(internalServerErrorMessage);
+  }
+}
+
+
+export async function deletePromoCode(req: Request, res: Response) {
+  const { shoppingCartID} = req.body;
+  try {
+    //error
+    if (!shoppingCartID || typeof shoppingCartID != "string") {
+      return res
+        .status(400)
+        .json(badRequestErrorMessage("Missing body parameter shoppingCartID"));
+    }  
+
+    await removePromoCode(shoppingCartID);
+    return res.status(200).send("");
+  } catch (error) {
+    return res.status(500).json(internalServerErrorMessage);
+  }
+}
+ 
