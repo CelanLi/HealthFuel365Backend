160557e8434ab4609741ca4fd001056c6dcc1ba9
Liping Yang <ge38hon@mytum.de>
2023-06-24 21:49:29 +0800

    add checkAuthentication in shoppingcart & order

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import * as openfoodfactsController from "../controllers/openfoodfactsController";
-import * as orderController from "../controllers/orderController"
+import * as orderController from "../controllers/orderController";
 import { checkAuthentication } from "../middleware/middleware";
 
 const orderRoutes = express.Router();
 
-orderRoutes.get("/:id", openfoodfactsController.findProduct);
+orderRoutes.get(
+  "/:id",
+  checkAuthentication,
+  openfoodfactsController.findProduct
+);
 
-orderRoutes.post("/createOrder", orderController.createOrder);
-
+orderRoutes.post(
+  "/createOrder",
+  checkAuthentication,
+  orderController.createOrder
+);
 
 export default orderRoutes;
-
- 
-
