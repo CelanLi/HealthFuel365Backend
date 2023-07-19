e000d67bc5767dac13d6c355f5be1f62a58741f9
Yiheng Zhu <ge38rob@mytum.de>
2023-07-19 15:26:10 +0200

    openfoodfacts folder remove

--- src/routes/openfoodfactsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as openfoodfactsController from "../controllers/openfoodfactsController";
-import { checkAuthentication } from "../middleware/middleware";
-
-const openfoodfactsRoutes = express.Router();
-
-openfoodfactsRoutes.get("/:id", openfoodfactsController.findProduct);
-
-export default openfoodfactsRoutes;
